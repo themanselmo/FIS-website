48693118d1eb6175b134e3db2750b587c03fb100
max anselmo <itmanselmo@gmail.com>
2021-09-16 11:45:39 -0400

    added a files directory for pictures and other assets, a nav and about me section, and fixed the add project script

--- js/project-btn.js
+++ js/project-btn.js
@@ -1,18 +1,20 @@
 function btnEventListener(){
     const btn = document.getElementById("project-button");
-    
-    let projectCounter = 4;
-    const p = document.createElement("p");
-    p.textContent = `Project ${projectCounter}`;
-    
     const projectList = document.getElementById("project-cont");
+    let projectCounter = 4;
     
+    
+    
+
     function addProject(){
-        projectList.append(p);
+        const p = document.createElement("p");
+        p.textContent = `Project ${projectCounter}`;
+        projectList.appendChild(p);
         projectCounter++;
+        console.log(`I was pressed ${projectCounter} time(s)`);
     }
 
-    btn.addEventListener('click', addProject);
+    btn.addEventListener("click", addProject);
 }
 
 btnEventListener();
